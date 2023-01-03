3d2017689a7c1b3a5a8b945cd9973d129e7f635b
Robin Mehner <robin@coding-robin.de>
2023-01-03 12:25:17 +0100

    Support Ruby and allow to keep latest minor versions

--- index.js
+++ index.js
@@ -1,31 +1,56 @@
-import { clean, parse, major, lt, eq } from "semver";
+import { clean, parse, lt, eq } from "semver";
 import { execSync } from "node:child_process";
 
-const installedVersions = execSync("asdf list nodejs", { encoding: "utf-8" })
-  .split("\n")
-  .map((version) => version.replace("*", ""))
-  .map((version) => clean(version))
-  .filter((version) => version !== null);
+const config = {
+  nodejs: "major",
+  ruby: "minor",
+};
 
-const versionsToKeep = installedVersions.reduce((toKeep, installedVersion) => {
-  const { version, major } = parse(installedVersion);
+const versionTokenFor = (language, version) => {
+  const { major, minor } = parse(version);
 
-  if (!toKeep.has(major)) {
-    toKeep.set(major, version);
+  return config[language] === "minor" ? `${major}.${minor}` : major;
+};
+
+Object.keys(config).forEach((language) => {
+  const installedVersions = execSync(`asdf list ${language}`, {
+    encoding: "utf-8",
+  })
+    .split("\n")
+    .map((version) => version.replace("*", ""))
+    .map((version) => clean(version))
+    .filter((version) => version !== null);
+
+  const versionsToKeep = installedVersions.reduce(
+    (toKeep, installedVersion) => {
+      const { version } = parse(installedVersion);
+      const versionToken = versionTokenFor(language, installedVersion);
+
+      if (!toKeep.has(versionToken)) {
+        toKeep.set(versionToken, version);
+      }
+
+      if (lt(toKeep.get(versionToken), version)) {
+        toKeep.set(versionToken, version);
+      }
+
+      return toKeep;
+    },
+    new Map()
+  );
+
+  console.log(language, installedVersions, versionsToKeep);
+
+  for (const installedVersion of installedVersions) {
+    const versionToken = versionTokenFor(language, installedVersion);
+
+    const versionToKeep = versionsToKeep.get(versionToken);
+
+    if (!eq(installedVersion, versionToKeep)) {
+      console.log(`Uninstalling ${language} ${installedVersion}`);
+      execSync(`asdf uninstall ${language} ${installedVersion}`);
+    } else {
+      console.log(`Keeping ${language} ${installedVersion}`);
+    }
   }
-
-  if (lt(toKeep.get(major), version)) {
-    toKeep.set(major, version);
-  }
-  return toKeep;
-}, new Map());
-
-for (const version of installedVersions) {
-  const majorVersion = major(version);
-  const versionToKeep = versionsToKeep.get(majorVersion);
-
-  if (!eq(version, versionToKeep)) {
-    console.log(`Uninstalling ${version}`);
-    execSync(`asdf uninstall nodejs ${version}`);
-  }
-}
+});
